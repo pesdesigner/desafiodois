44712f3d0c831db76f3caf3741b6d6b4deee68e3
pesdesigner <pauloeduardo.dg@outlook.com>
2024-11-24 15:30:47 -0300

    Project created

---
+++ src/main/java/com/graficoeweb/desafiodois/DesafiodoisApplication.java
@@ -0,0 +1,13 @@
+package com.graficoeweb.desafiodois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiodoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiodoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graficoeweb/desafiodois/DesafiodoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graficoeweb.desafiodois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiodoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
